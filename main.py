e96f0b836d3c0bc78f0e2cefe4bee25afc918b7a
Hlebéts <glib.kobryn@gmail.com>
2023-11-08 20:05:25 +0200

    Add tag search functionality to main.py and update text.txt

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-open("text.txt", "r")
+while True:
+    main_input = input("Введіть назву тегу, який вам необхідний: ").lower()
+    tagged_input = "#" + main_input
+    file = open("text.txt", "r")
 
-input("Введіть назву тегу, який вам необхідний:")
+    try:
+        with file:
+            found_lines = [line for line in file if tagged_input in line.lower()]
 
+        if found_lines:
+            print("Знайдено наступні рядки: \n")
+            for line in found_lines:
+                print(line)
+        elif main_input == "exit":
+            break
+        else:
+            print("Такого тегу не існує")
+    except:
+        print("Помилка відкриття файлу")
